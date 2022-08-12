0574b3599b8ca93fb5ca5e73c11874cdb5d54fbc
Abdulazizms <abdulaziizmohammeds@gmail.com>
2022-08-12 10:05:38 +0300

    add class attributes and methods

--- src/main/java/com/mycompany/performancecomparison/Subscriber.java
+++ src/main/java/com/mycompany/performancecomparison/Subscriber.java
@@ -9,5 +9,20 @@ package com.mycompany.performancecomparison;
  * @author Abdulaziz Al-Alshaikh
  */
 public class Subscriber {
+    private long shares;
+    private final int id;
+    
+    public Subscriber(int id, long shares){
+        this.id = id;
+        this.shares = shares;
+    }
+    
+    public long getShares(){
+        return this.shares;
+    }
+    
+    public void addShares(long newShares){
+        this.shares += newShares;
+    }
     
 }
