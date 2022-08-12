28afd414709d501ca575a769e24c3c41745aa842
Abdulazizms <abdulaziizmohammeds@gmail.com>
2022-08-12 10:35:45 +0300

    add necessary static fields

--- src/main/java/com/mycompany/performancecomparison/Database.java
+++ src/main/java/com/mycompany/performancecomparison/Database.java
@@ -12,9 +12,12 @@ public class Database {
     
     private Subscriber [] subscribers;
     public static int MAX_OPERATIONS;
+    public static int MAX_USER_ID;
+
     public Database(Subscriber [] subscribers, int maxOperations){
         this.subscribers = subscribers;
         MAX_OPERATIONS = maxOperations;
+        MAX_USER_ID = subscribers.length - 1;
     }
     
     
